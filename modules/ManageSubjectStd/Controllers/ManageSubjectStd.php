15ac99a921138cce6924784106b46bbcd397e7a2
Chonlawit penwijit <DSITSY@gmail.com>
2024-06-26 21:52:51 +0700

    update

--- modules/ManageSubjectStd/Controllers/ManageSubjectStd.php
+++ modules/ManageSubjectStd/Controllers/ManageSubjectStd.php
@@ -15,11 +15,11 @@ class ManageSubjectStd extends BaseController
   public function SubjectStdList()
   {
     $ManageSubjectStdModel = new ManageSubjectStdModel();
-    $department = $_GET['department'];
-    $id_card = $_GET['id_card'];
+    $department = @$_GET['department'];
+    $id_card = @$_GET['id_card'];
     // $data['department_get'] = $_GET['department'];
-    $year = $_GET['year'];
-    $room = $_GET['room'];
+    $year = @$_GET['year'];
+    $room = @$_GET['room'];
     $student_id = '';
     $data['listStudent'] = $ManageSubjectStdModel->getListStudent($student_id, $department, $id_card, $year, $room);
     $data['department'] = $ManageSubjectStdModel->getDepartment();
