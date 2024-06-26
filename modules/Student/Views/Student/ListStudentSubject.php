541aa1e18e518d70007a13aa9c27f4a9f780b3fa
Chonlawit penwijit <DSITSY@gmail.com>
2024-06-26 22:15:36 +0700

    update

--- modules/Student/Views/Student/ListStudentSubject.php
+++ modules/Student/Views/Student/ListStudentSubject.php
@@ -24,13 +24,13 @@
                                 <div class="col-lg-12">
                                     <div class="row">
                                         <div class="col-lg-12 text-start">
-                                            ชื่อ-นามสกุล <?php echo $student['student_name_th'] . ' ' . $student['student_lname_th'] ?> ชื่อเล่น <?php echo $student['student_nickname']  ?>
+                                            ชื่อ-นามสกุล <?php echo @$student['student_name_th'] . ' ' . @$student['student_lname_th'] ?> ชื่อเล่น <?php echo @$student['student_nickname']  ?>
                                         </div>
                                         <div class="col-lg-12 text-start">
-                                            ที่อยู่ <?php echo $student['student_address'] . ' หมู่ ' . $student['student_moo'] . ' จังหวัด ' . $student['std_province'] . ' อำเภอ ' . $student['std_district'] . ' จังหวัด ' . $student['std_subdistrict']  ?>
+                                            ที่อยู่ <?php echo @$student['student_address'] . ' หมู่ ' . @$student['student_moo'] . ' จังหวัด ' . @$student['std_province'] . ' อำเภอ ' . @$student['std_district'] . ' จังหวัด ' . @$student['std_subdistrict']  ?>
                                         </div>
                                         <div class="col-lg-12 text-start">
-                                            เบอร์ติดต่อนักศึกษา <?php echo $student['student_phone'] ?>
+                                            เบอร์ติดต่อนักศึกษา <?php echo @$student['student_phone'] ?>
                                         </div>
                                     </div>
                                 </div>
@@ -80,58 +80,58 @@
                                     <tbody>
                                         <?php
                                         foreach ($student_subject as $key => $row) {
-                                            $sum_unit1 += $row['unit'];
+                                            $sum_unit1 += @$row['unit'];
 
                                             $textGrade1 = '';
-                                            if ($row['grade_student'] > 80) {
+                                            if (@$row['grade_student'] > 80) {
                                                 $textGrade1 = '4';
-                                            } else if ($row['grade_student'] >= 75) {
+                                            } else if (@$row['grade_student'] >= 75) {
                                                 $textGrade1 = '3.5';
-                                            } else if ($row['grade_student'] >= 70) {
+                                            } else if (@$row['grade_student'] >= 70) {
                                                 $textGrade1 = '3';
-                                            } else if ($row['grade_student'] >= 65) {
+                                            } else if (@$row['grade_student'] >= 65) {
                                                 $textGrade1 = '2.5';
-                                            } else if ($row['grade_student'] >= 60) {
+                                            } else if (@$row['grade_student'] >= 60) {
                                                 $textGrade1 = '2';
-                                            } else if ($row['grade_student'] >= 55) {
+                                            } else if (@$row['grade_student'] >= 55) {
                                                 $textGrade1 = '1.5';
-                                            } else if ($row['grade_student'] >= 50) {
+                                            } else if (@$row['grade_student'] >= 50) {
                                                 $textGrade1 = '1';
-                                            } else if ($row['grade_student'] < 50) {
+                                            } else if (@$row['grade_student'] < 50) {
                                                 $textGrade1 = '0';
                                             } else {
                                                 $textGrade1 = '-';
                                             }
 
-                                            $sum_ResutlUnit1 += $textGrade1 * $row['unit'];
+                                            $sum_ResutlUnit1 += @$textGrade1 * $row['unit'];
                                         ?>
                                             <tr>
                                                 <td class="text-center">
-                                                    <?php echo $key + 1 ?>
+                                                    <?php echo @$key + 1 ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['subjects_id'] ?>
+                                                    <?php echo @$row['subjects_id'] ?>
                                                 </td>
                                                 <td>
-                                                    <?php echo $row['name'] ?>
+                                                    <?php echo @$row['name'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['lecture_unit'] ?>
+                                                    <?php echo @$row['lecture_unit'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['practical_unit'] ?>
+                                                    <?php echo @$row['practical_unit'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['hour'] ?>
+                                                    <?php echo @$row['hour'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $textGrade1 ?>
+                                                    <?php echo @$textGrade1 ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['unit'] ?>
+                                                    <?php echo @$row['unit'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $textGrade1 * $row['unit'] ?>
+                                                    <?php echo @$textGrade1 * @$row['unit'] ?>
                                                 </td>
                                             </tr>
                                         <?php
@@ -144,15 +144,15 @@
                                                 รวม
                                             </td>
                                             <td class="text-center">
-                                                <?php echo $sum_unit1 ?>
+                                                <?php echo @$sum_unit1 ?>
                                             </td>
                                             <td class="text-center">
-                                                <?php echo $sum_ResutlUnit1 ?>
+                                                <?php echo @$sum_ResutlUnit1 ?>
                                             </td>
                                         </tr>
                                         <tr>
                                             <td colspan="9" class="text-center">
-                                                เกรดเฉลี่ยประจำภาคเรียน 1/<?php echo date('Y') + 543 ?> = <?php echo $sum_ResutlUnit1 / $sum_unit1 ?>
+                                                เกรดเฉลี่ยประจำภาคเรียน 1/<?php echo date('Y') + 543 ?> = <?php echo @$sum_ResutlUnit1 / @$sum_unit1 ?>
                                             </td>
                                         </tr>
                                     </tfoot>
@@ -207,57 +207,57 @@
                                     <tbody>
                                         <?php
                                         foreach ($student_subject2 as $key => $row) {
-                                            $sum_unit += $row['unit'];
+                                            $sum_unit += @$row['unit'];
 
                                             $textGrade = '';
-                                            if ($row['grade_student'] > 80) {
+                                            if (@$row['grade_student'] > 80) {
                                                 $textGrade = '4';
-                                            } else if ($row['grade_student'] >= 75) {
+                                            } else if (@$row['grade_student'] >= 75) {
                                                 $textGrade = '3.5';
-                                            } else if ($row['grade_student'] >= 70) {
+                                            } else if (@$row['grade_student'] >= 70) {
                                                 $textGrade = '3';
-                                            } else if ($row['grade_student'] >= 65) {
+                                            } else if (@$row['grade_student'] >= 65) {
                                                 $textGrade = '2.5';
-                                            } else if ($row['grade_student'] >= 60) {
+                                            } else if (@$row['grade_student'] >= 60) {
                                                 $textGrade = '2';
-                                            } else if ($row['grade_student'] >= 55) {
+                                            } else if (@$row['grade_student'] >= 55) {
                                                 $textGrade = '1.5';
-                                            } else if ($row['grade_student'] >= 50) {
+                                            } else if (@$row['grade_student'] >= 50) {
                                                 $textGrade = '1';
-                                            } else if ($row['grade_student'] < 50) {
+                                            } else if (@$row['grade_student'] < 50) {
                                                 $textGrade = '0';
                                             } else {
                                                 $textGrade = '-';
                                             }
-                                            $sum_ResutlUnit += $textGrade * $row['unit'];
+                                            $sum_ResutlUnit += @$textGrade * @$row['unit'];
                                         ?>
                                             <tr>
                                                 <td class="text-center">
-                                                    <?php echo $key + 1 ?>
+                                                    <?php echo @$key + 1 ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['subjects_id'] ?>
+                                                    <?php echo @$row['subjects_id'] ?>
                                                 </td>
                                                 <td>
-                                                    <?php echo $row['name'] ?>
+                                                    <?php echo @$row['name'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['lecture_unit'] ?>
+                                                    <?php echo @$row['lecture_unit'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['practical_unit'] ?>
+                                                    <?php echo @$row['practical_unit'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['hour'] ?>
+                                                    <?php echo @$row['hour'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $textGrade ?>
+                                                    <?php echo @$textGrade ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $row['unit'] ?>
+                                                    <?php echo @$row['unit'] ?>
                                                 </td>
                                                 <td class="text-center">
-                                                    <?php echo $textGrade * $row['unit'] ?>
+                                                    <?php echo @$textGrade * @$row['unit'] ?>
                                                 </td>
                                             </tr>
                                         <?php
@@ -270,15 +270,15 @@
                                                 รวม
                                             </td>
                                             <td class="text-center">
-                                                <?php echo $sum_unit ?>
+                                                <?php echo @$sum_unit ?>
                                             </td>
                                             <td class="text-center">
-                                                <?php echo $sum_ResutlUnit ?>
+                                                <?php echo @$sum_ResutlUnit ?>
                                             </td>
                                         </tr>
                                         <tr>
                                             <td colspan="9" class="text-center">
-                                                เกรดเฉลี่ยประจำภาคเรียน 2/<?php echo date('Y') + 543 ?> = <?php echo $sum_ResutlUnit / $sum_unit ?>
+                                                เกรดเฉลี่ยประจำภาคเรียน 2/<?php echo date('Y') + 543 ?> = <?php echo @$sum_ResutlUnit / @$sum_unit ?>
                                             </td>
                                         </tr>
                                     </tfoot>
