be940abdd378b13c3d49dcd07de6f6efc32a2d32
Chonlawit penwijit <DSITSY@gmail.com>
2024-06-26 22:21:03 +0700

    backup

--- app/Views/template/layout.php
+++ app/Views/template/layout.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="<?php echo base_url('public/img/logofve.png') ?>">
     <title>Pre-Ved Fve Home Page</title>
     <link rel="stylesheet" href="<?php echo base_url('public/css/css.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('public/css/style.css.map') ?>">
